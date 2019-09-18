2cd502bbbcf593e29369c0abe613a0ba277aa243
Théo Senoussaoui <theosen95@gmail.com>
2019-09-18 20:46:46 +0200

    test gonic

--- back/app.go
+++ back/app.go
@@ -1,20 +1,34 @@
 package main
 
-import (
-    "net/http"
-    "log"
-    "github.com/gorilla/mux"
-)
+// import (
+//     "net/http"
+//     "log"
+//     "github.com/gorilla/mux"
+// )
 
-func YourHandler(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Gorilla!\n"))
-}
+// func YourHandler(w http.ResponseWriter, r *http.Request) {
+//     w.Write([]byte("Gorilla!\n"))
+// }
+
+// func main() {
+//     r := mux.NewRouter()
+//     // Routes consist of a path and a handler function.
+//     r.HandleFunc("/", YourHandler)
+
+//     // Bind to a port and pass our router in
+//     log.Fatal(http.ListenAndServe(":8080", r))
+// }
+
+// Test ajout Gonic
+
+import "github.com/gin-gonic/gin"
 
 func main() {
-    r := mux.NewRouter()
-    // Routes consist of a path and a handler function.
-    r.HandleFunc("/", YourHandler)
-
-    // Bind to a port and pass our router in
-    log.Fatal(http.ListenAndServe(":8080", r))
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
--- back/go.mod
+++ back/go.mod
@@ -2,7 +2,7 @@ module github.com/theosenoussaoui/go_project
 
 require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.4.0 // indirect
+	github.com/gin-gonic/gin v1.4.0
 	github.com/golang/protobuf v1.3.2 // indirect
 	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/mux v1.6.2
@@ -11,3 +11,5 @@ require (
 	github.com/ugorji/go v1.1.7 // indirect
 	golang.org/x/sys v0.0.0-20190916202348-b4ddaad3f8a3 // indirect
 )
+
+go 1.13
