43fa8e78c1f03471a6b7bfb7dbccfc3f3a772cf4
Théo Senoussaoui <theosen95@gmail.com>
2019-09-22 22:19:02 +0200

    modification app et readme pour todos

--- front/README.md
+++ front/README.md
@@ -27,3 +27,11 @@ yarn run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### Todolist on the front-end : 
+- Barre de navigation
+- Accueil avec une liste des propositions en cours 
+- Page de détails d'une proposition ?
+- Page de login et d'inscription
+- Bouton de déconnexion
+- Création de nouvelle proposition si loggé en tant qu'admin
--- front/src/App.vue
+++ front/src/App.vue
@@ -1,76 +1,57 @@
 <template>
-  <el-row>
-    <el-form ref="form" :model="form" label-width="120px">
-      <el-form-item label="Activity name">
-        <el-input v-model="form.name"></el-input>
-      </el-form-item>
-      <el-form-item label="Activity zone">
-        <el-select v-model="form.region" placeholder="please select your zone">
-          <el-option label="Zone one" value="shanghai"></el-option>
-          <el-option label="Zone two" value="beijing"></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="Activity time">
-        <el-col :span="11">
-          <el-date-picker
-            type="date"
-            placeholder="Choisissez une date"
-            v-model="form.date1"
-            style="width: 100%;"
-          ></el-date-picker>
-        </el-col>
-        <el-col class="line" :span="2">-</el-col>
-        <el-col :span="11">
-          <el-time-picker placeholder="Pick a time" v-model="form.date2" style="width: 100%;"></el-time-picker>
-        </el-col>
-      </el-form-item>
-      <el-form-item label="Instant delivery">
-        <el-switch v-model="form.delivery"></el-switch>
-      </el-form-item>
-      <el-form-item label="Activity type">
-        <el-checkbox-group v-model="form.type">
-          <el-checkbox label="Online activities" name="type"></el-checkbox>
-          <el-checkbox label="Promotion activities" name="type"></el-checkbox>
-          <el-checkbox label="Offline activities" name="type"></el-checkbox>
-          <el-checkbox label="Simple brand exposure" name="type"></el-checkbox>
-        </el-checkbox-group>
-      </el-form-item>
-      <el-form-item label="Resources">
-        <el-radio-group v-model="form.resource">
-          <el-radio label="Sponsor"></el-radio>
-          <el-radio label="Venue"></el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="Activity form">
-        <el-input type="textarea" v-model="form.desc"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">Créer</el-button>
-        <el-button>Annuler</el-button>
-      </el-form-item>
-    </el-form>
-  </el-row>
+  <div>
+    <el-row>
+      <Navbar></Navbar>
+    </el-row>
+    <el-container>
+      <CardList></CardList>
+    </el-container>
+  </div>
 </template>
 
 <script>
+import Navbar from "./components/Navbar.vue";
+import CardList from "./components/CardList.vue";
 export default {
   name: "app",
-  components: {},
+  components: {
+    Navbar,
+    CardList
+  },
   data() {
     return {
-      activeIndex: "1",
-      activeIndex2: "1"
+      form: {
+        name: "",
+        region: "",
+        date1: "",
+        date2: "",
+        delivery: false,
+        type: [],
+        resource: "",
+        desc: ""
+      }
     };
+  },
+  methods: {
+    onSubmit() {
+      console.log("submit!");
+    }
   }
 };
 </script>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+* {
+  font-family: "system-ui" !important;
 }
 </style>
