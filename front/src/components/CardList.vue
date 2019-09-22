c76523dc77c212d5590cea232c359b3b53d44d43
Théo Senoussaoui <theosen95@gmail.com>
2019-09-22 22:20:04 +0200

    adding card components

---
+++ front/src/components/CardList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <el-row>
+      <Card></Card>
+    </el-row>
+    <el-row>
+      <Card></Card>
+    </el-row>
+    <el-row>
+      <Card></Card>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import Card from "./Card.vue";
+export default {
+  name: "app",
+  components: {
+    Card
+  },
+  data() {
+    return {
+      form: {
+        name: "",
+        region: "",
+        date1: "",
+        date2: "",
+        delivery: false,
+        type: [],
+        resource: "",
+        desc: ""
+      }
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log("submit!");
+    }
+  }
+};
+</script>
+
+<style>
+</style>
