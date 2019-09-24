f4b41770df5e8c4f65edc55dd083095efd423fcc
Quentin Denisot <quentindenisot@orange.fr>
2019-09-24 23:58:33 +0200

    [WIP] routes

--- back/app.go
+++ back/app.go
@@ -25,10 +25,48 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
+	
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
+	r.POST("/users", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "POST users",
+		})
+	})
+
+	r.PUT("/users/:uuid", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "PUT users",
+		})
+	})
+
+	r.DELETE("/users/:uuid", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "DELETE users",
+		})
+	})
+
+	r.POST("/votes", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "POST votes",
+		})
+	})
+
+	r.GET("/votes/:uuid", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "GET votes",
+		})
+	})
+
+	r.PUT("/votes/:uuid", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "PUT votes",
+		})
+	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
