91a72a892fa0bb29501fc93886acbbd3ce64c322
Théo Senoussaoui <theosen95@gmail.com>
2019-09-29 00:33:09 +0200

    fixing app.go main exemple

--- back/app.go
+++ back/app.go
@@ -1,34 +1,46 @@
 package main
 
-// import (
-//     "net/http"
-//     "log"
-//     "github.com/gorilla/mux"
-// )
+import (
+	"log"
 
-// func YourHandler(w http.ResponseWriter, r *http.Request) {
-//     w.Write([]byte("Gorilla!\n"))
-// }
-
-// func main() {
-//     r := mux.NewRouter()
-//     // Routes consist of a path and a handler function.
-//     r.HandleFunc("/", YourHandler)
-
-//     // Bind to a port and pass our router in
-//     log.Fatal(http.ListenAndServe(":8080", r))
-// }
-
-// Test ajout Gonic
-
-import "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
+	"github.com/theosenoussaoui/project_go/back/controllers"
+	"github.com/theosenoussaoui/project_go/back/db"
+	"github.com/theosenoussaoui/project_go/back/middleware"
+)
 
 func main() {
+	log.Println("Starting server...")
+
+	db.Initialize()
+	db.CreateSystemAdmin()
+
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	route := r.Group("/")
+
+	// Manage login (auth + generate JWT)
+	authMiddleware, err := middleware.AuthMiddleware()
+	if err != nil {
+		log.Fatal("JWT Error:" + err.Error())
+	}
+
+	// route.POST("/login", authMiddleware.LoginHandler)
+
+	// Manage protected routes
+	route.Use(authMiddleware.MiddlewareFunc())
+	{
+		users := route.Group("/users")
+		{
+			users.GET("/", controllers.GetUsers)
+			users.POST("/", controllers.CreateUser)
+		}
+
+		// votes := route.Group("/votes")
+		// {
+		// 	votes.GET("/", controllers.GetVotes)
+		// 	votes.POST("/", controllers.CreateVote)
+		// }
+	}
+
+	r.Run(":8080")
 }
