b1cd452e14230406c7bdfe948bad1c3579dd79fd
theosenoussaoui <theosen95@gmail.com>
2019-11-17 22:06:28 +0100

    Fixing routes with required layouts...

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+import VotePage from "../views/VotePage.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +12,21 @@ const routes = [
     path: "/",
     name: "home",
     component: Home
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: Login
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: Register
+  },
+  {
+    path: "/vote",
+    name: "votepage",
+    component: VotePage
   },
   {
     path: "/about",
---
+++ front/src/views/VotePage.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="votepage">
+        <h1>This is the page for voting page</h1>
+    </div>
+</template>
