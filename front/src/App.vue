0b4240cc402dd155d9de3e4813de82d067569ada
Quentin Denisot <quentindenisot@orange.fr>
2020-01-28 22:55:31 +0100

    [fix] alignement menu

--- front/src/App.vue
+++ front/src/App.vue
@@ -2,21 +2,25 @@
     <div id="app">
         <div id="nav">
             <el-menu class="el-menu-demo" mode="horizontal">
-                <el-menu-item>
-                    <router-link to="/">Home</router-link>
-                </el-menu-item>
-                <el-menu-item>
-                    <router-link to="/about">About</router-link>
-                </el-menu-item>
-                <el-menu-item>
-                    <router-link to="/login">Login</router-link>
-                </el-menu-item>
-                <el-menu-item>
-                    <router-link to="/register">Register</router-link>
-                </el-menu-item>
-                <el-menu-item>
-                    <router-link to="/vote">Vote</router-link>
-                </el-menu-item>
+                <div id="menuLeft">
+                    <el-menu-item>
+                        <router-link to="/">Home</router-link>
+                    </el-menu-item>
+                </div>
+                <div id="menuRight">
+                    <el-menu-item>
+                        <router-link to="/about">About</router-link>
+                    </el-menu-item>
+                    <el-menu-item>
+                        <router-link to="/vote">Vote</router-link>
+                    </el-menu-item>
+                    <el-menu-item id="login-menu-item">
+                        <router-link to="/login">Login</router-link>
+                    </el-menu-item>
+                    <el-menu-item id="register-menu-item">
+                        <router-link to="/register">Register</router-link>
+                    </el-menu-item>
+                </div>
             </el-menu>
         </div>
         <router-view />
@@ -24,7 +28,8 @@
 </template>
 
 <style lang="scss">
-#app {
+#app
+{
     color: #2c3e50;
     font-family: "Avenir", Helvetica, Arial, sans-serif;
     text-align: center;
@@ -32,16 +37,37 @@
     -webkit-font-smoothing: antialiased;
 }
 
-#nav {
+#nav
+{
     padding-bottom: 30px;
 
-    a {
-        color: #2c3e50;
-        font-weight: bold;
-        text-decoration: none;
+    .el-menu::before,
+    .el-menu::after
+    {
+        content: none;
+    }
 
-        &.router-link-exact-active {
-            color: #42b983;
+    ul
+    {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+
+        #menuRight
+        {
+            display: flex;
+        }
+
+        a
+        {
+            color: #2c3e50;
+            font-weight: bold;
+            text-decoration: none;
+
+            &.router-link-exact-active
+            {
+                color: #42b983;
+            }
         }
     }
 }
